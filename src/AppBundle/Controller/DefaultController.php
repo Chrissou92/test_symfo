458b81f33a5036293f1e7fd4787ea93a67363204
Chrissou <nicolchristelle@gmail.com>
2018-09-15 11:07:28 +0200

    createView form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,15 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+        // create form
+        $form = $this->createFormBuilder()
+        ->add('firstName', TextType::class)
+        ->add('lastName', TextType::class)
+        ->add('numEtud', TextType::class)
+        ->getForm();
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        return $this->render('default/index.html.twig', array('form' => $form->createView(),
+    ));
     }
 }
